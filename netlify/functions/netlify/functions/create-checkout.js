42e67b5f9c684249dad1b6c1b529b95b7fd4ccf8
nathanialjaspergleeson-beep <nathanialjaspergleeson@gmail.com>
2026-07-08 01:01:05 +1000

    Create create-checkout.js

---
+++ netlify/functions/netlify/functions/create-checkout.js
@@ -0,0 +1,35 @@
+const Stripe = require("stripe");
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  try {
+    const { priceId } = JSON.parse(event.body);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      success_url: `${process.env.URL}/success`,
+      cancel_url: `${process.env.URL}/cancel`,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        url: session.url,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
+};
